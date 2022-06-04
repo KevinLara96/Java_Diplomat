49092dd83042f6a388e1ee9c664f8bb7baf48dc1
KevinLara96 <l_kevin96@hotmail.com>
2022-06-03 22:41:15 -0500

    Remove irrelevant files

--- Modulo1/tercera-entrega/src/Juego.java
+++ Modulo1/tercera-entrega/src/Juego.java
@@ -1,8 +1,77 @@
+import java.util.Scanner;
+
 public class Juego {
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
         Personaje personaje1 = new Personaje("Personaje 1", 25, 300);
         Personaje personaje2 = new Personaje("Personaje 2", 50, 150);
 
-        System.out.println("Hola");
+        System.out.println("Bienvenido al juego. \n Los controles son los siguientes:");
+        System.out.println("1. Atacar con personaje 1.");
+        System.out.println("2. Defensa con personaje 1.");
+        System.out.println("3. Atacar con personaje 2.");
+        System.out.println("4. Defensa con personaje 2.");
+
+        int jugar = 1;
+
+        do {
+            System.out.print("Jugada: ");
+            int accion = input.nextInt();
+
+            switch (accion) {
+                case 1:
+                    personaje1.atacar(personaje2); // P1 ataca a P2.
+
+                    // Se muestra la vida de los jugadores.
+                    System.out.println("Vida P1: " + personaje1.getVida());
+                    System.out.println("Vida P2: " + personaje2.getVida());
+                    break;
+
+                case 2:
+                    // Si P1 está en ataque, pasa a defensa y viceversa.
+                    personaje1.setDefensa(!personaje1.isDefensa());
+                    if (personaje1.isDefensa())
+                        System.out.println("P1 en modo defensa.");
+                    else
+                        System.out.println("P1 en modo ataque.");
+                    break;
+
+                case 3:
+                    personaje2.atacar(personaje1); // P2 ataca a P1.
+
+                    // Se muestra la vida de los jugadores.
+                    System.out.println("Vida P1: " + personaje1.getVida());
+                    System.out.println("Vida P2: " + personaje2.getVida());
+                    break;
+
+                case 4:
+                    // Si P2 está en ataque, pasa a defensa y vicecersa.
+                    personaje2.setDefensa(!personaje2.isDefensa());
+                    if (personaje2.isDefensa())
+                        System.out.println("P2 en modo defensa.");
+                    else
+                        System.out.println("P2 en modo ataque.");
+                    break;
+            }
+
+            if (!personaje1.isVivo()) { // Ganó P2.
+                System.out.println("Ganó personaje 2.");
+            } else if (!personaje2.isVivo()) { // Ganó P1.
+                System.out.println("Ganó personaje 1.");
+            }
+
+            // Si ambos siguen vivos, se sigue jugando.
+            if (personaje1.isVivo() && personaje2.isVivo()) {
+                continue;
+            }
+
+            System.out.println("¿Desea volver a jugar?");
+            System.out.println("1. -> Sí.");
+            System.out.println("2. -> No.");
+            System.out.print("Su respuesta: ");
+            jugar = input.nextInt();
+
+        } while (jugar == 1);
     }
 }
--- Modulo1/tercera-entrega/src/Personaje.java
+++ Modulo1/tercera-entrega/src/Personaje.java
@@ -1,13 +1,10 @@
-import java.security.PublicKey;
-
-import javax.print.DocFlavor.STRING;
-
 public class Personaje {
     String nombre;
     int fuerza;
-    int vida;
+    double vida;
     boolean vivo;
     int posicion;
+    boolean Defensa;
 
     public Personaje(String nombre, int fuerza, int vida) {
         this.nombre = nombre;
@@ -16,8 +13,62 @@ public class Personaje {
         this.vivo = true;
     }
 
-    public atacar(Personaje oponente){
-        if(oponente.isDefensa())
-            oponente.
+    public void atacar(Personaje oponente) {
+        if (oponente.isDefensa())
+            // Si el oponente se está cubriendo, solo se resta un porcentaje de vida.
+            oponente.setVida(oponente.getVida() - (this.fuerza * 0.25));
+        else
+            // Si el oponente no se está cubriendo, se resta el total del ataque.
+            oponente.setVida(oponente.getVida() - this.fuerza);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getFuerza() {
+        return fuerza;
+    }
+
+    public void setFuerza(int fuerza) {
+        this.fuerza = fuerza;
+    }
+
+    public double getVida() {
+        return vida;
+    }
+
+    public void setVida(double vida) {
+        this.vida = vida;
+        if (this.getVida() <= 0)
+            this.setVivo(false);
+    }
+
+    public boolean isVivo() {
+        return vivo;
+    }
+
+    public void setVivo(boolean vivo) {
+        this.vivo = vivo;
+    }
+
+    public int getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(int posicion) {
+        this.posicion = posicion;
+    }
+
+    public boolean isDefensa() {
+        return Defensa;
+    }
+
+    public void setDefensa(boolean defensa) {
+        Defensa = defensa;
     }
 }
