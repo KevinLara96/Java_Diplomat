ef655d5c4a5b7e48d4c9a400e1e576c69518c72d
KevinLara96 <l_kevin96@hotmail.com>
2022-09-23 21:01:41 -0500

    a

--- Modulo7/Practicas/Practica4/m07s4/src/main/java/mx/unam/dgtic/m07s3/AlumnoController.java
+++ Modulo7/Practicas/Practica4/m07s4/src/main/java/mx/unam/dgtic/m07s3/AlumnoController.java
@@ -1,10 +1,22 @@
 package mx.unam.dgtic.m07s3;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping
+@RequestMapping("/alumnos")
 public class AlumnoController {
 
+    @Autowired
+    AlumnoRepositorio repositorioAlumno;
+
+    @RequestMapping(value = "buscarTodos")
+    public String buscarTodos(Model modelo) {
+        Iterable<Alumno> iterable = repositorioAlumno.findAll();
+        modelo.addAttribute("alumnos", iterable);
+
+        return "alumnos"; // Nombre del archivo HTML.
+    }
 }
