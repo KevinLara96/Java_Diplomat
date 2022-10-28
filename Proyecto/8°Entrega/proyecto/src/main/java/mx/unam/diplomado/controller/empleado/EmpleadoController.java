0d3cada0d64689da167b07048cb05ad1233124e8
KevinLara96 <l_kevin96@hotmail.com>
2022-10-27 20:03:05 -0500

    Remove autowired annotations

--- Proyecto/8°Entrega/proyecto/src/main/java/mx/unam/diplomado/controller/empleado/EmpleadoController.java
+++ Proyecto/8°Entrega/proyecto/src/main/java/mx/unam/diplomado/controller/empleado/EmpleadoController.java
@@ -1,9 +1,15 @@
 package mx.unam.diplomado.controller.empleado;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import mx.unam.diplomado.modelo.entidades.empleado.Empleado;
 import mx.unam.diplomado.service.servicioAgencia.AgenciaService;
 import mx.unam.diplomado.service.servicioEmpleado.EmpleadoService;
 import mx.unam.diplomado.service.servicioPuesto.PuestoService;
@@ -12,14 +18,24 @@ import mx.unam.diplomado.service.servicioPuesto.PuestoService;
 @RequestMapping("/tmp")
 public class EmpleadoController {
 
-    /*
-     * @Autowired
-     * private EmpleadoService empleadoService;
-     * 
-     * @Autowired
-     * private AgenciaService agenciaService;
-     * 
-     * @Autowired
-     * private PuestoService puestoService;
-     */
+    // @Autowired
+    private EmpleadoService empleadoService;
+
+    // @Autowired
+    private AgenciaService agenciaService;
+
+    // @Autowired
+    private PuestoService puestoService;
+
+    @ResponseBody
+    @RequestMapping(value = { "/jsonEmpleado" })
+    public List<Empleado> tipoEmpleadoJson() {
+        return null;
+    }
+
+    @RequestMapping(value = "/listar", method = RequestMethod.GET)
+    public String cargaEmpleado(Model model) {
+        model.addAttribute("empleados", empleadoService.cargaEmpleados());
+        return "listaEmpleados";
+    }
 }
