ddf9056deddba0d316bd1c922ed3c5246cdf4453
KevinLara96 <l_kevin96@hotmail.com>
2022-10-27 20:46:28 -0500

    Finish Employee Controller

--- Proyecto/8°Entrega/proyecto/src/main/java/mx/unam/diplomado/controller/empleado/EmpleadoController.java
+++ Proyecto/8°Entrega/proyecto/src/main/java/mx/unam/diplomado/controller/empleado/EmpleadoController.java
@@ -1,24 +1,25 @@
 package mx.unam.diplomado.controller.empleado;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import javax.validation.Valid;
 
-import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import mx.unam.diplomado.forms.empleado.EmpleadoForm;
 import mx.unam.diplomado.modelo.entidades.agencia.Agencia;
 import mx.unam.diplomado.modelo.entidades.empleado.Empleado;
+import mx.unam.diplomado.modelo.entidades.puesto.Puesto;
 import mx.unam.diplomado.service.servicioAgencia.AgenciaService;
 import mx.unam.diplomado.service.servicioEmpleado.EmpleadoService;
 import mx.unam.diplomado.service.servicioPuesto.PuestoService;
@@ -54,79 +55,59 @@ public class EmpleadoController {
         return "empleadoForm";
     }
 
-    /*
-     * @RequestMapping(value = "/guardaEmpleado", method = RequestMethod.POST)
-     * public ModelAndView guardaEmpleado(@ModelAttribute("empleado") @Valid
-     * EmpleadoForm empleadoForm,
-     * BindingResult result) {
-     * ModelAndView vista = new ModelAndView();
-     * if (result.hasErrors()) {
-     * vista.addObject(empleadoForm);
-     * vista.setViewName("empleadoForm");
-     * } else {
-     * Empleado empleado = empleadoForm.getId() != null ?
-     * empleadoService.cargaEmpleado(empleadoForm.getId())
-     * : new Empleado();
-     * empleado.setNombre(empleadoForm.getNombre());
-     * empleado.setCorreo(empleadoForm.getCorreo());
-     * empleado.setContrasena(empleadoForm.getContrasena());
-     * empleado.setRfc(empleadoForm.getRfc());
-     * empleado.setSalario(empleadoForm.getSalario());
-     * 
-     * Agencia agencia = agenciaService.cargaPorNombre(empleadoForm.getAgencia());
-     * empleado.setTipoEmpleado(tipoEmpleado);
-     * empleado.setDireccion(empleadoForm.getDireccion());
-     * empleadoService.guardaEmpleado(empleado);
-     * 
-     * Set<EmpleadoMedio> empleadosMedio = new HashSet<EmpleadoMedio>();
-     * MedioEmpleado medioEmpleado =
-     * puestoService.cargaPorNombre(empleadoForm.getMedioEmpleado());
-     * EmpleadoMedio empleadoMedio = new EmpleadoMedio();
-     * empleadoMedio.setEmpleado(empleado);
-     * empleadoMedio.setMedioEmpleado(medioEmpleado);
-     * empleadoMedio.setValor(empleadoForm.getMedio());
-     * empleadosMedio.add(empleadoMedio);
-     * empleado.setEmpleadosMedios(empleadosMedio);
-     * empleadoService.guardaEmpleado(empleado);
-     * vista.addObject("empleados", empleadoService.cargaEmpleados());
-     * vista.setViewName("listaEmpleados");
-     * }
-     * return vista;
-     * 
-     * }
-     * 
-     * @RequestMapping("/actualizar/{idEmpleado}")
-     * public String showFormForUpdate(@PathVariable Integer idEmpleado, Model
-     * model) {
-     * Empleado empleado = empleadoService.cargaEmpleado(idEmpleado);
-     * if (empleado != null) {
-     * EmpleadoForm empleadoForm = new EmpleadoForm();
-     * empleadoForm.setId(idEmpleado);
-     * empleadoForm.setNombre(empleado.getNombre());
-     * empleadoForm.setApellidos(empleado.getApellidos());
-     * empleadoForm.setEdad(empleado.getEdad());
-     * empleadoForm.setDireccion(empleado.getDireccion());
-     * empleadoForm.setTipoEmpleado(empleado.getTipoEmpleado().getNombre());
-     * Set<EmpleadoMedio> empleadosMedio = empleado.getEmpleadosMedios();
-     * if (empleadosMedio != null && !empleadosMedio.isEmpty()) {
-     * for (Iterator<EmpleadoMedio> it = empleadosMedio.iterator(); it.hasNext();) {
-     * EmpleadoMedio empleadoMedio = it.next();
-     * empleadoForm.setMedioEmpleado(empleadoMedio.getMedioEmpleado().getNombre());
-     * empleadoForm.setMedio(empleadoMedio.getValor());
-     * }
-     * }
-     * model.addAttribute("empleado", empleadoForm);
-     * } else {
-     * model.addAttribute("empleado", new EmpleadoForm());
-     * }
-     * return "empleadoForm";
-     * }
-     * 
-     * @RequestMapping("eliminar")
-     * public String eliminaEmpleado(@RequestParam("empleadoId") Integer idEmpleado)
-     * {
-     * empleadoService.eliminaEmpleado(idEmpleado);
-     * return "redirect:/empleados/listar";
-     * }
-     */
+    @RequestMapping(value = "/guardaEmpleado", method = RequestMethod.POST)
+    public ModelAndView guardaEmpleado(@ModelAttribute("empleado") @Valid EmpleadoForm empleadoForm,
+            BindingResult result) {
+        ModelAndView vista = new ModelAndView();
+        if (result.hasErrors()) {
+            vista.addObject(empleadoForm);
+            vista.setViewName("empleadoForm");
+        } else {
+            Empleado empleado = empleadoForm.getIdEmpleado() != null
+                    ? empleadoService.cargaEmpleado(empleadoForm.getIdEmpleado()).get()
+                    : new Empleado();
+            empleado.setIdEmpleado((empleadoForm.getIdEmpleado()));
+            empleado.setNombre(empleadoForm.getNombre());
+            empleado.setCorreo(empleadoForm.getCorreo());
+            empleado.setContrasena(empleadoForm.getContrasena());
+            empleado.setRfc(empleadoForm.getRfc());
+            empleado.setSalario(empleadoForm.getSalario());
+            Agencia agencia = agenciaService.cargaPorNombre(empleadoForm.getAgencia());
+            empleado.setAgencia(agencia);
+            Puesto puesto = puestoService.cargaPorNombre(empleadoForm.getPuesto());
+            empleado.setPuesto(puesto);
+
+            empleadoService.guardaEmpleado(empleado);
+            vista.addObject("empleados", empleadoService.cargaEmpleados());
+            vista.setViewName("listaEmpleados");
+        }
+        return vista;
+
+    }
+
+    @RequestMapping("/actualizar/{idEmpleado}")
+    public String showFormForUpdate(@PathVariable Integer idEmpleado, Model model) {
+        Empleado empleado = empleadoService.cargaEmpleado(idEmpleado).get();
+        if (empleado != null) {
+            EmpleadoForm empleadoForm = new EmpleadoForm();
+            empleadoForm.setIdEmpleado(idEmpleado);
+            empleadoForm.setNombre(empleado.getNombre());
+            empleadoForm.setCorreo(empleado.getCorreo());
+            empleadoForm.setContrasena(empleado.getContrasena());
+            empleadoForm.setRfc(empleado.getRfc());
+            empleadoForm.setAgencia(empleado.getAgencia().getNombreAgencia());
+            empleadoForm.setPuesto(empleado.getPuesto().getPuesto());
+            model.addAttribute("empleado", empleadoForm);
+        } else {
+            model.addAttribute("empleado", new EmpleadoForm());
+        }
+        return "empleadoForm";
+    }
+
+    @RequestMapping("eliminar")
+    public String eliminaEmpleado(@RequestParam("empleadoId") Integer idEmpleado) {
+        empleadoService.eliminaEmpleado(idEmpleado);
+        return "redirect:/empleados/listar";
+    }
+
 }
