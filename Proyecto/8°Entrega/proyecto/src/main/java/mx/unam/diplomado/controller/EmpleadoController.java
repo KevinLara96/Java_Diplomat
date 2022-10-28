f8e6df8fd60715fb50618d06425f2f7624deef5f
KevinLara96 <l_kevin96@hotmail.com>
2022-10-28 00:02:40 -0500

    Update repo

--- Proyecto/8°Entrega/proyecto/src/main/java/mx/unam/diplomado/controller/EmpleadoController.java
+++ Proyecto/8°Entrega/proyecto/src/main/java/mx/unam/diplomado/controller/EmpleadoController.java
@@ -1,7 +1,15 @@
 package mx.unam.diplomado.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import mx.unam.diplomado.forms.EmpleadoForm;
+import mx.unam.diplomado.modelo.entidades.Empleado;
 import mx.unam.diplomado.service.AgenciaService;
 import mx.unam.diplomado.service.EmpleadoService;
 import mx.unam.diplomado.service.PuestoService;
@@ -17,4 +25,23 @@ public class EmpleadoController {
     @Autowired
     private PuestoService puestoService;
 
+    @ResponseBody
+    @RequestMapping(value = { "/jsonEmpleado" })
+    public List<Empleado> tipoEmpleadoJson() {
+        return empleadoService.cargaEmpleados();
+    }
+
+    @RequestMapping(value = "/listar", method = RequestMethod.GET)
+    public String cargcaEmpleado(Model model) {
+        model.addAttribute("empleados", empleadoService.cargaEmpleados());
+
+        return "listaEmpleados";
+    }
+
+    @RequestMapping("/empleadoForm")
+    public String empleadoForm(Model model) {
+        model.addAttribute("empleado", new EmpleadoForm());
+
+        return "empleadoForm";
+    }
 }
