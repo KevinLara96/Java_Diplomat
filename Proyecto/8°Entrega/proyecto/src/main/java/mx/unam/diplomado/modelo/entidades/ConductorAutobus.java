b539d12b8db0e751de44987238d2a130aadee856
KevinLara96 <l_kevin96@hotmail.com>
2022-10-27 23:15:07 -0500

    Start changing package organization

---
+++ Proyecto/8°Entrega/proyecto/src/main/java/mx/unam/diplomado/modelo/entidades/ConductorAutobus.java
@@ -0,0 +1,56 @@
+package mx.unam.diplomado.modelo.entidades;
+
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import mx.unam.diplomado.modelo.entidades.agencia.Agencia;
+
+@Entity
+@Table(name = "conductor_autobus")
+public class ConductorAutobus {
+
+    @Id
+    private Integer idAsignacionAutobus;
+    private Integer idConductor;
+    private Integer idAutobus;
+
+    @ManyToOne
+    @JoinColumn(name = "idAgencia")
+    private Agencia agencia;
+
+    public Integer getIdAsignacionAutobus() {
+        return idAsignacionAutobus;
+    }
+
+    public void setIdAsignacionAutobus(Integer idAsignacionAutobus) {
+        this.idAsignacionAutobus = idAsignacionAutobus;
+    }
+
+    public Integer getIdConductor() {
+        return idConductor;
+    }
+
+    public void setIdConductor(Integer idConductor) {
+        this.idConductor = idConductor;
+    }
+
+    public Integer getIdAutobus() {
+        return idAutobus;
+    }
+
+    public void setIdAutobus(Integer idAutobus) {
+        this.idAutobus = idAutobus;
+    }
+
+    public Agencia getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(Agencia agencia) {
+        this.agencia = agencia;
+    }
+
+}
