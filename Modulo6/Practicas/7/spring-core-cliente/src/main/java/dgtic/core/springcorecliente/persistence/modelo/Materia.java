3d992712a663a345f142ba38d9a7c4a76c6b1124
KevinLara96 <l_kevin96@hotmail.com>
2022-11-08 04:50:28 -0600

    Update repo

---
+++ Modulo6/Practicas/7/spring-core-cliente/src/main/java/dgtic/core/springcorecliente/persistence/modelo/Materia.java
@@ -0,0 +1,45 @@
+package dgtic.core.springcorecliente.persistence.modelo;
+
+public class Materia {
+    private Integer id;
+    private String nombre;
+    private int creditos;
+
+    public Materia() {
+    }
+
+    public Materia(String nombre, int creditos) {
+        super();
+        this.nombre = nombre;
+        this.creditos = creditos;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCreditos() {
+        return creditos;
+    }
+
+    public void setCreditos(int creditos) {
+        this.creditos = creditos;
+    }
+
+    @Override
+    public String toString() {
+        return "Materia [id=" + id + ", nombre=" + nombre + ", creditos=" + creditos + "]";
+    }
+}
