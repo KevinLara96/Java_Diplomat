c89ba18a354b2f8abe10947ab5150f994b0ee6aa
KevinLara96 <l_kevin96@hotmail.com>
2022-11-10 18:11:55 -0600

    Finish module 6 exercise

--- Modulo6/Practicas/5/spring-core-restbd/src/main/java/dgtic/core/springcorerestbd/service/MicroServicios.java
+++ Modulo6/Practicas/5/spring-core-restbd/src/main/java/dgtic/core/springcorerestbd/service/MicroServicios.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import dgtic.core.springcorerestbd.persistence.dao.EstudianteDao;
@@ -18,13 +21,23 @@ public class MicroServicios {
     @Autowired
     private EstudianteDao estudiante;
 
-    @GetMapping(value = "estudiantes", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/estudiantes", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Estudiante> servicioUno() {
         return estudiante.consulta();
     }
 
-    @DeleteMapping(value = "borrar/{cuenta}")
+    @DeleteMapping(value = "/borrar/{cuenta}", produces = MediaType.APPLICATION_JSON_VALUE)
     public void eliminar(@PathVariable("cuenta") String cuenta) {
         estudiante.borrar(estudiante.consultaId(cuenta));
+    }
+
+    @PutMapping(value = "/cambiar", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void cambiarEstudiante(@RequestBody Estudiante estudianteNuevo) {
+        estudiante.cambiar(estudianteNuevo);
+    }
+
+    @PostMapping(value = "/nuevo", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void nuevoEstudiante(@RequestBody Estudiante estudianteNuevo) {
+        estudiante.insertar(estudianteNuevo);
     }
 }
