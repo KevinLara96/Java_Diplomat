512fe4c012d5b79929fc670b06996c6a17b46d79
KevinLara96 <l_kevin96@hotmail.com>
2022-11-12 12:43:27 -0600

    Finish base exercise

---
+++ Modulo11/Practicas/1/m1100base/src/main/java/unam/dgtic/diplomado/m1100base/dominio/Employee.java
@@ -0,0 +1,85 @@
+package unam.dgtic.diplomado.m1100base.dominio;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Employee")
+public class Employee {
+
+    @Id
+    private Integer id;
+    private String name;
+    private Long salary;
+
+    public Employee() {
+
+    }
+
+    public Employee(Integer id) {
+        super();
+        this.id = id;
+    }
+
+    public Employee(Integer id, String name, Long salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Long salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Employee other = (Employee) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+}
---
+++ Modulo11/Practicas/2/m1101camposypropiedades/src/main/java/unam/dgtic/diplomado/m1100base/modelo/EmployeeService.java
@@ -0,0 +1,62 @@
+package unam.dgtic.diplomado.m1100base.modelo;
+
+import java.util.Collection;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
+import unam.dgtic.diplomado.m1100base.dominio.Employee;
+
+public class EmployeeService {
+
+    protected EntityManager em;
+
+    public EmployeeService(EntityManager em) {
+        this.em = em;
+    }
+
+    public Employee findEmployee(Integer id) {
+
+        return em.find(Employee.class, id);
+    }
+
+    @SuppressWarnings("unchecked")
+    public Collection<Employee> findAllEmployees() {
+        Query query = em.createQuery("SELECT e from Employee e");
+
+        return (Collection<Employee>) query.getResultList();
+    }
+
+    public Employee createEmployee(Integer id, String name, Long salario) {
+
+        Employee employee = new Employee();
+        employee.setId(id);
+        employee.setName(name);
+        employee.setSalary(salario);
+
+        em.getTransaction().begin();
+        em.persist(employee);
+        em.getTransaction().commit();
+
+        return employee;
+    }
+
+    public void removeEmployee(Integer id) {
+        Employee employee = findEmployee(id);
+        if (employee != null) {
+            em.getTransaction().begin();
+            em.remove(employee);
+            em.getTransaction().commit();
+        }
+    }
+
+    public Employee updateEmployee(Employee employeeParam) {
+        Employee employee = findEmployee(employeeParam.getId());
+        if (employee != null) {
+            em.getTransaction().begin();
+            employee = em.merge(employeeParam);
+            em.getTransaction().commit();
+        }
+
+        return employee;
+    }
+}
