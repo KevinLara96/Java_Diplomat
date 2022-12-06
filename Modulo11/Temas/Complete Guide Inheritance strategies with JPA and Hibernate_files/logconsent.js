d632e83f449bfb6f00327c5788cc20da39b4c5e0
KevinLara96 <l_kevin96@hotmail.com>
2022-12-06 02:32:47 -0600

    Add JPA notes

---
+++ Modulo11/Temas/Complete Guide Inheritance strategies with JPA and Hibernate_files/logconsent.js
@@ -0,0 +1,5 @@
+CookieConsent.setCookie("{stamp:%27ltzJHA22dpbnWKFzZ4sn6d+Q8X0+m6CiRfxun+oTJ6io2QBwcYWxzg==%27%2Cnecessary:true%2Cpreferences:false%2Cstatistics:false%2Cmarketing:false%2Cver:5%2Cutc:1670308361606%2Cregion:%27mx%27}", new Date(1701844361606), '/', '', '');
+if (typeof CookieConsent.removeBulkReset!='undefined') {CookieConsent.removeBulkReset();}
+CookieConsent.userCountry = 'mx';
+CookieConsent.init();
+if (typeof CookieConsent.resetCookies!='undefined') {CookieConsent.resetCookies();}
