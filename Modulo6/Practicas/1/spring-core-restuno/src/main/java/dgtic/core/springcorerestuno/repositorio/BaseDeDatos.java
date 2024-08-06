4fbec82fc7445244fbe831c889756d1a1036fc24
KevinLara96 <l_kevin96@hotmail.com>
2024-08-06 14:52:02 -0600

    Update format

--- Modulo6/Practicas/1/spring-core-restuno/src/main/java/dgtic/core/springcorerestuno/repositorio/BaseDeDatos.java
+++ Modulo6/Practicas/1/spring-core-restuno/src/main/java/dgtic/core/springcorerestuno/repositorio/BaseDeDatos.java
@@ -11,14 +11,20 @@ public class BaseDeDatos {
     public static List<Estudiante> estudiante = new ArrayList<>();
 
     static {
-        Estudiante est = new Estudiante("A1", "Rosa", "19", new Materia(1, "Lógica", 10),
+        Estudiante est = new Estudiante(
+                "A1", "Rosa", "19",
+                new Materia(1, "Lógica", 10),
                 new Materia(1, "Programación", 9));
 
-        est = new Estudiante("A2", "Mario", "18", new Materia(1, "Lógica", 10),
+        est = new Estudiante(
+                "A2", "Mario", "18",
+                new Materia(1, "Lógica", 10),
                 new Materia(1, "Programación", 9),
                 new Materia(3, "Álgebra", 9));
 
-        est = new Estudiante("A3", "Rebeca", "22", new Materia(1, "Lógica", 10),
+        est = new Estudiante(
+                "A3", "Rebeca", "22",
+                new Materia(1, "Lógica", 10),
                 new Materia(3, "Álgebra", 9));
     }
 }
